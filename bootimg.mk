5849edcdfe39d0e439e654e3edfae373a3269ca1
ROD129JO <107906021+ROD129JO@users.noreply.github.com>
2026-08-30 13:37:19 -0600

    Fix recovery image target dependencies in bootimg.mk

--- bootimg.mk
+++ bootimg.mk
@@ -6,7 +6,7 @@ ifdef TARGET_PREBUILT_DTB
 	BOARD_MKBOOTIMG_ARGS += --dt $(TARGET_PREBUILT_DTB)
 endif
 
-$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) $(recovery_kernel) $(recovery_ramdisk)
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_kernel) $(recovery_ramdisk)
 	@echo -e ${CL_GRN}"----- Making recovery image ------"${CL_RST}
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@ --ramdisk $(recovery_ramdisk)
 	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
